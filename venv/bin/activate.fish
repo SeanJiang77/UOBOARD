f962507a3519ea00633049fba7fd1e94c95752c7
Sean Jiang <SeanJiang2005@gmail.com>
2024-09-13 18:17:21 -0400

    update requirements

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Volumes/UOboardgame/MyProject/venv"
+set -gx VIRTUAL_ENV "/Users/seanjiang/Documents/GitHub/UOBOARD/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
